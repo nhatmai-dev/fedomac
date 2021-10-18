5c37b784de09cc2ee322d0e584ef59989edae696
unknown <lamhv@greenglobal.vn>
2021-10-18 10:05:26 +0700

    feat(ui): Fedomac-008 display articles in knowledge base page

--- packages/ui/src/pages/admin/knowledge-base/articles/index.tsx
+++ packages/ui/src/pages/admin/knowledge-base/articles/index.tsx
@@ -1,45 +1,88 @@
 import styles from './index.less';
-import { Table, Tag, Space } from 'antd';
+import { Table, Tag, Space,Button } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
+
 
 const dataSource = [
   {
     key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, dignissimos?',
+    tags: ["Lorem","ipsum","dolor"],
+    author: "Lam",
+    createdDate: "10/17/2021"
   },
   {
     key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
+    title: 'Ipsum dolor sit amet consectetur adipisicing elit. Nulla, dignissimos?',
+    tags: ["Lorem","ipsum","dolor"],
+    author: "Nhat",
+    createdDate: "10/14/2021"
   },
+  {
+    key: '3',
+    title: 'Ipsum dolor sit amet consectetur adipisicing elit. Nulla, dignissimos?',
+    tags: ["Lorem","ipsum","dolor"],
+    author: "Hoang",
+    createdDate: "10/13/2021"
+  }
 ];
 
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title'
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: 'Tags',
+    dataIndex: 'tags',
+    key: 'tags',
+    render: (tags:[]) => (
+      <>
+        {
+          tags.map(tag => {
+           return(
+              <Tag key={tag}>
+                {tag}
+              </Tag>
+           )
+          })
+        } 
+      </>
+    )
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'Author',
+    dataIndex: 'author',
+    key: 'author'
   },
+  {
+    title: 'Created Date',
+    dataIndex: 'createdDate',
+    key: 'created date'
+  },
+  {
+    title: 'Action',
+    dataIndex: 'action',
+    key: 'action',
+    render: () => (
+      <Space>
+          <a>
+            Edit
+          </a>
+          <a>
+            Delete
+          </a>
+      </Space>
+    )
+  }
 ];
 
-
+const pageHeader = {extra: [<Button key="1">Create New Article</Button>]}
 
 export default function KnowledgeBasePage() {
   return (
-    <PageContainer>
+    <PageContainer header={pageHeader}>
       <Table dataSource={dataSource} columns={columns} />
     </PageContainer>
   );
